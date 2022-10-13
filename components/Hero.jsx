ba4f008b80c28eac072b94c597460d41f4e7d5e8
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-10-13 18:56:40 -0500

    Added meta & tailwind themes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
             <div class="flex justify-start lg:w-0 lg:flex-1">
               <a href="#">
                 <span class="sr-only">Your Company</span>
-                <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600" alt="" />
               </a>
             </div>
             <nav class="hidden space-x-10 sm:flex">
@@ -20,7 +20,7 @@ export const Hero = () => {
             </nav>
             <div class="items-center justify-end flex flex-1 lg:w-0">
               <a href="#" class="hidden sm:flex whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
-              <a href="#" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
+              <a href="#" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-brand-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-brand-700">Sign up</a>
             </div>
           </div>
         </div>
@@ -31,15 +31,15 @@ export const Hero = () => {
           <div class="px-4 sm:px-8 lg:w-1/2 xl:pr-16">
             <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
               <span class="block xl:inline">Data to enrich your</span>
-              <span class="block text-indigo-600 xl:inline">online business</span>
+              <span class="block text-brand-600 xl:inline">online business</span>
             </h1>
             <p class="mx-auto mt-3 max-w-md text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
             <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
               <div class="rounded-md shadow">
-                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Get started</a>
+                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-brand-600 px-8 py-3 text-base font-medium text-white hover:bg-brand-700 md:py-4 md:px-10 md:text-lg">Get started</a>
               </div>
               <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-indigo-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg">Live demo</a>
+                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-brand-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg">Live demo</a>
               </div>
             </div>
           </div>
--- components/Layout.js
+++ components/Layout.js
@@ -3,9 +3,43 @@ import { html } from 'hono/html'
 export const Layout = ({children}) => html`<!doctype html>
 <html>
   <head>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta charset="UTF-8" />
+    <title>{{ site.name }} - {{ site.title }}</title>
+
+    <meta name="description" content="{{ site.description }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <meta itemprop="name" content="{{ site.name }} - {{ site.title }}" />
+    <meta itemprop="description" content="{{ site.description }}" />
+    <meta itemprop="image" content="{{ site.image | default: 'https://opengraph.githubassets.com/0b7b051d3a37b58ffe0b3c9abc1f8c179c0e698c70135a53186224ec1b1c7b56/nathanclevenger/ship' }}" />
+
+    <meta property="og:url" content="https://{{ site.name | default: 'landing.page.as' }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{{ site.name }} - {{ site.title }}" />
+    <meta property="og:description" content="{{ site.description }}" />
+    <meta property="og:image" content="{{ site.image | default: 'https://opengraph.githubassets.com/0b7b051d3a37b58ffe0b3c9abc1f8c179c0e698c70135a53186224ec1b1c7b56/nathanclevenger/ship' }}" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="{{ site.name }} - {{ site.title }}" />
+    <meta name="twitter:description" content="{{ site.description }}" />
+    <meta name="twitter:image" content="{{ site.image | default: 'https://opengraph.githubassets.com/0b7b051d3a37b58ffe0b3c9abc1f8c179c0e698c70135a53186224ec1b1c7b56/nathanclevenger/ship' }}" /></>
+      
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            screens: {
+              'xs': '475px',
+            },
+            colors: {
+              brand: tailwind.colors.blue,
+            }
+          }
+        }
+      }
+    </script>
   </head>
   <body>${children}</body>
 </html>`
