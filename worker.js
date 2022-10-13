0de099e54cd55a9640a3a43e4d07baba3c968e27
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-10-13 18:02:23 -0500

    Cleanup

--- worker.js
+++ worker.js
@@ -1,30 +1,9 @@
 import { Hono } from 'hono'
-
 import { Layout } from './components/Layout'
 import { Hero } from './components/Hero'
 
 const app = new Hono()
 
-
-
-const Top = ({messages}) => {
-  return (
-    <Layout>
-      <h1>Hello Hono!</h1>
-      <ul>
-        {messages.map((message) => {
-          return <li>{message}!!</li>
-        })}
-      </ul>
-    </Layout>
-  )
-}
-
-app.get('/', c => {
-  // const messages = ['Good Morning', 'Good Evening', 'Good Night']
-  // return c.html(Page(<Top messages={messages} />))
-
-  return c.html(<Layout><Hero/></Layout>)
-})
+app.get('/', c => c.html(<Layout><Hero/></Layout>))
 
 export default app
