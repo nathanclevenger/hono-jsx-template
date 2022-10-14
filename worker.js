bdf230e2e1635bc6f29d7c7ce9d08d6e8514cc34
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-10-14 07:00:22 -0500

    Updated dashboard

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { Dashboard } from './components/Dashboard'
 import { getContext } from './context'
 import { LandingPage } from './LandingPage'
 
@@ -6,6 +7,7 @@ const app = new Hono()
 
 app.use('*', getContext)
 app.get('/', c => c.html(<LandingPage />))
+app.get('/app', c => c.html(<Dashboard />))
 app.get('/api', c => c.json({ hello: 'world' }))
 
 export default app
