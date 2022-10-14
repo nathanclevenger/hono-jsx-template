15fa319d969fbeeae1f84ccdd1f3f72e66374ff6
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-10-14 06:04:00 -0500

    Added pricing and FAQ

--- LandingPage.jsx
+++ LandingPage.jsx
@@ -5,14 +5,16 @@ import { Benefits } from './components/Benefits'
 import { Stats } from './components/Stats'
 import { Posts } from './components/Posts'
 import { Logos } from './components/Logos'
+import { Pricing } from './components/Pricing'
 
 export const LandingPage = props => (
   <Layout {...props} >
     <Hero {...props} />
     <Stats {...props} />
     <Benefits {...props} />
-    <Logos />
+    <Logos {...props} />
     <Features {...props} />
+    <Pricing {...props} />
     <Posts {...props} />
   </Layout>
 )
