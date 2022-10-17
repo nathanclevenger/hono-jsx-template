9c01fd1d881b497da813daa676694e9dad18bed9
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-10-17 03:33:19 -0500

    Updated layout

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -137,9 +137,9 @@ export const Dashboard = props => (
               <Form />
               {/* { props.children } */}
               { /* Replace with your content */ }
-              <div class="py-4">
+              {/* <div class="py-4">
                 <div class="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-              </div>
+              </div> */}
               { /* /End replace */ }
             </div>
           </div>
